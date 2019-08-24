b1e2b622e7628e2bd2a3fa2b4ac99210979e3532
Rain Liu <yuliu@outlook.com>
2019-08-23 18:21:46 -0700

    add opus tests

--- rtp/src/codecs/opus.rs
+++ rtp/src/codecs/opus.rs
@@ -4,6 +4,9 @@ use std::io::Read;
 
 use utils::Error;
 
+#[cfg(test)]
+mod opus_test;
+
 struct Opus {
     payload: Vec<u8>,
 }
@@ -12,7 +15,11 @@ impl Payloader for Opus {
     fn payload<R: Read>(&self, mtu: isize, reader: &mut R) -> Result<Vec<Vec<u8>>, Error> {
         let mut payload = vec![];
         reader.read_to_end(&mut payload)?;
-        Ok(vec![payload])
+        if payload.is_empty() {
+            Ok(vec![])
+        } else {
+            Ok(vec![payload])
+        }
     }
 }
 
@@ -20,6 +27,10 @@ impl Depacketizer for Opus {
     fn depacketize<R: Read>(&mut self, reader: &mut R) -> Result<(), Error> {
         self.payload.clear();
         reader.read_to_end(&mut self.payload)?;
-        Ok(())
+        if self.payload.is_empty() {
+            Err(Error::new("Payload is not large enough".to_string()))
+        } else {
+            Ok(())
+        }
     }
 }
