52683c92190ca5356aaa3c1d3749f327c71c06fc
Rain Liu <yuliu@outlook.com>
2020-09-17 23:00:12 -0700

    add transport_cc_extension and test

---
+++ rtp/src/extension/transport_cc_extension/transport_cc_extension_test.rs
@@ -0,0 +1,48 @@
+use super::*;
+
+use std::io::{BufReader, BufWriter};
+
+use util::Error;
+
+#[test]
+fn test_transport_cc_extension_too_small() -> Result<(), Error> {
+    let raw: Vec<u8> = vec![];
+    let mut reader = BufReader::new(raw.as_slice());
+    let result = TransportCCExtension::unmarshal(&mut reader);
+    assert!(result.is_err());
+
+    Ok(())
+}
+
+#[test]
+fn test_transport_cc_extension() -> Result<(), Error> {
+    let raw: Vec<u8> = vec![0x00, 0x02];
+    let mut reader = BufReader::new(raw.as_slice());
+    let t1 = TransportCCExtension::unmarshal(&mut reader)?;
+    let t2 = TransportCCExtension {
+        transport_sequence: 2,
+    };
+    assert_eq!(t1, t2);
+
+    let mut dst: Vec<u8> = vec![];
+    {
+        let mut writer = BufWriter::<&mut Vec<u8>>::new(dst.as_mut());
+        t2.marshal(&mut writer)?;
+    }
+    assert_eq!(raw, dst);
+
+    Ok(())
+}
+
+#[test]
+fn test_transport_cc_extension_extra_bytes() -> Result<(), Error> {
+    let raw: Vec<u8> = vec![0x00, 0x02, 0x00, 0xff, 0xff];
+    let mut reader = BufReader::new(raw.as_slice());
+    let t1 = TransportCCExtension::unmarshal(&mut reader)?;
+    let t2 = TransportCCExtension {
+        transport_sequence: 2,
+    };
+    assert_eq!(t1, t2);
+
+    Ok(())
+}
