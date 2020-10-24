6a07026aeeb3e7ac6f1367ab1d9101a70f094e71
Rain Liu <yuliu@outlook.com>
2020-10-24 12:22:10 -0700

    add p256 in named_curve

--- dtls/src/curve/named_curve.rs
+++ dtls/src/curve/named_curve.rs
@@ -1,3 +1,9 @@
+use rand_core::OsRng; // requires 'getrandom' feature
+
+use util::Error;
+
+use crate::errors::*;
+
 // https://www.iana.org/assignments/tls-parameters/tls-parameters.xml#tls-parameters-8
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum NamedCurve {
@@ -23,34 +29,55 @@ pub struct NamedCurveKeypair {
     public_key: Vec<u8>,
     private_key: Vec<u8>,
 }
-/*
-fn elliptic_curve_keypair(curve: NamedCurve, c1:elliptic.Curve
-                          c2: elliptic.Curve) ->Result<NamedCurveKeypair, Error> {
-    private_key, x, y, err := elliptic.GenerateKey(c1, rand.Reader)
-    if err != nil {
-        return nil, err
-    }
 
-    Ok(NamedCurveKeypair{curve, elliptic.Marshal(c2, x, y), private_key})
+fn elliptic_curve_keypair(curve: NamedCurve) -> Result<NamedCurveKeypair, Error> {
+    let (public_key, private_key) = match curve {
+        NamedCurve::P256 => {
+            let secret_key = elliptic_curve::SecretKey::<p256::NistP256>::random(&mut OsRng);
+            let public_key = elliptic_curve::sec1::EncodedPoint::<p256::NistP256>::from_secret_key(
+                &secret_key,
+                false,
+            );
+            (
+                public_key.as_bytes().to_vec(),
+                secret_key.to_bytes().to_vec(),
+            )
+        }
+        /*NamedCurve::P384 => {
+            let secret_key = elliptic_curve::SecretKey::<p384::NistP384>::random(&mut OsRng);
+            let public_key = elliptic_curve::sec1::EncodedPoint::<p384::NistP384>::from_secret_key(
+                &secret_key,
+                false,
+            );
+        }*/
+        _ => return Err(ERR_INVALID_NAMED_CURVE.clone()),
+    };
+
+    Ok(NamedCurveKeypair {
+        curve,
+        public_key,
+        private_key,
+    })
 }
 
-impl NamedCurve{
-    pub fn generate_keypair(&self) ->Result<NamedCurveKeypair, Error> {
-        match *c {
-         /*NamedCurve::X25519=>{
-            tmp := make([]byte, 32)
-            if _, err := rand.Read(tmp); err != nil {
-                return nil, err
-            }
+impl NamedCurve {
+    pub fn generate_keypair(&self) -> Result<NamedCurveKeypair, Error> {
+        match *self {
+            /*NamedCurve::X25519=>{
+                tmp := make([]byte, 32)
+                if _, err := rand.Read(tmp); err != nil {
+                    return nil, err
+                }
 
-            var public, private [32]byte
-            copy(private[:], tmp)
+                var public, private [32]byte
+                copy(private[:], tmp)
 
-            curve25519.ScalarBaseMult(&public, &private)
-            Ok(NamedCurveKeypair{curve:NamedCurve::X25519, public_key, private_key})
-        }*/
-        NamedCurve::P256 => elliptic_curve_keypair(NamedCurve::P256, elliptic.P256(), elliptic.P256()),
-        NamedCurve::P384 => elliptic_curve_keypair(NamedCurve::P384, elliptic.P384(), elliptic.P384()),
-        _=> Err(ERR_INVALID_NAMED_CURVE.clone()),
+                curve25519.ScalarBaseMult(&public, &private)
+                Ok(NamedCurveKeypair{curve:NamedCurve::X25519, public_key, private_key})
+            }*/
+            NamedCurve::P256 => elliptic_curve_keypair(NamedCurve::P256),
+            //NamedCurve::P384 => elliptic_curve_keypair(NamedCurve::P384),
+            _ => Err(ERR_INVALID_NAMED_CURVE.clone()),
+        }
     }
-}*/
+}
