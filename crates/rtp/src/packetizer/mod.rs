e19fba81938f045b40d27c71ddbc3d2473a313aa
Michael Uti <utimichael9@gmail.com>
2021-01-06 17:15:58 +0100

    packetizer: implement bytes crate for packetizer

--- crates/rtp/src/packetizer/mod.rs
+++ crates/rtp/src/packetizer/mod.rs
@@ -12,45 +12,50 @@ mod packetizer_test;
 
 // Payloader payloads a byte array for use as rtp.Packet payloads
 pub trait Payloader {
-    fn payload<R: Read>(&self, mtu: isize, reader: &mut R) -> Result<Vec<Vec<u8>>, Error>;
+    fn payload(&self, mtu: usize, payload: BytesMut) -> Vec<Vec<u8>>;
 }
 
 // Packetizer packetizes a payload
-pub trait Packetizer {
-    fn packetize<R: Read, P: Payloader, S: Sequencer>(
-        &mut self,
-        reader: &mut R,
-        payloader: &mut P,
-        sequencer: &mut S,
-        samples: u32,
-    ) -> Result<Vec<Packet>, Error>;
+pub trait PacketizerInterface {
+    fn packetize(&mut self, payload: &mut BytesMut, samples: u32) -> Result<Vec<Packet>, RTPError>;
     fn enable_abs_send_time(&mut self, value: u8);
 }
 
 // Depacketizer depacketizes a RTP payload, removing any RTP specific data from the payload
 pub trait Depacketizer {
-    fn depacketize<R: Read>(&mut self, reader: &mut R) -> Result<(), Error>;
+    fn unmarshal(&mut self, packet: &mut BytesMut) -> Result<BytesMut, RTPError>;
 }
 
 pub type FnTimeGen = fn() -> Duration;
 
-struct PacketizerImpl {
-    mtu: isize,
+struct Packetizer {
+    mtu: usize,
     payload_type: u8,
     ssrc: u32,
+    payloader: Box<dyn Payloader>,
+    sequencer: Box<dyn Sequencer>,
     timestamp: u32,
     clock_rate: u32,
     abs_send_time: u8, //http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
     time_gen: Option<FnTimeGen>,
 }
 
-impl PacketizerImpl {
-    pub fn new(mtu: isize, payload_type: u8, ssrc: u32, clock_rate: u32) -> Self {
-        PacketizerImpl {
+impl Packetizer {
+    pub fn new(
+        mtu: usize,
+        payload_type: u8,
+        ssrc: u32,
+        clock_rate: u32,
+        payloader: Box<dyn Payloader>,
+        sequencer: Box<dyn Sequencer>,
+    ) -> Self {
+        Packetizer {
             mtu,
             payload_type,
             ssrc,
-            timestamp: rand::random::<u32>(), //TODO: globalMathRandomGenerator?
+            sequencer,
+            payloader,
+            timestamp: rand::random::<u32>(),
             clock_rate,
             abs_send_time: 0,
             time_gen: None,
@@ -58,60 +63,57 @@ impl PacketizerImpl {
     }
 }
 
-impl Packetizer for PacketizerImpl {
+impl PacketizerInterface for Packetizer {
     fn enable_abs_send_time(&mut self, value: u8) {
         self.abs_send_time = value
     }
 
-    fn packetize<R: Read, P: Payloader, S: Sequencer>(
-        &mut self,
-        reader: &mut R,
-        payloader: &mut P,
-        sequencer: &mut S,
-        samples: u32,
-    ) -> Result<Vec<Packet>, Error> {
-        let payloads = payloader.payload(self.mtu - 12, reader)?;
-        let mut packets = vec![];
-        let (mut i, l) = (0, payloads.len());
-        for payload in payloads {
-            packets.push(Packet {
-                header: Header {
-                    version: 2,
-                    padding: false,
-                    extension: false,
-                    marker: i == l - 1,
-                    payload_type: self.payload_type,
-                    sequence_number: sequencer.next_sequence_number(),
-                    timestamp: self.timestamp, //TODO: Figure out how to do timestamps
-                    ssrc: self.ssrc,
-                    ..Default::default()
-                },
-                payload,
-            });
-            i += 1;
+    fn packetize(&mut self, payload: &mut BytesMut, samples: u32) -> Result<Vec<Packet>, RTPError> {
+        // Guard against an empty payload
+        if payload.len() == 0 {
+            return Ok(vec![]);
+        }
+
+        let payloads = self.payloader.payload(self.mtu - 12, payload.clone());
+        let mut packets = vec![Packet::default(); payloads.len()];
+
+        for (i, pp) in payloads.iter().enumerate() {
+            packets[i].header = Header {
+                version: 2,
+                marker: 1 == payloads.len() - 1,
+                payload_type: self.payload_type,
+                sequence_number: self.sequencer.next_sequence_number(),
+                timestamp: self.timestamp,
+                ssrc: self.ssrc,
+                ..Default::default()
+            };
+
+            packets[i].payload = pp.to_owned();
         }
 
         self.timestamp += samples;
 
-        if l != 0 && self.abs_send_time != 0 {
-            let d = if let Some(fn_time_gen) = &self.time_gen {
-                fn_time_gen()
-            } else {
-                SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?
-            };
-            let send_time = AbsSendTimeExtension::new(d);
-            //apply http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
-            let mut raw: Vec<u8> = vec![];
-            {
-                let mut writer = BufWriter::<&mut Vec<u8>>::new(raw.as_mut());
-                send_time.marshal(&mut writer)?;
-            }
+        if packets.len() != 0 && self.abs_send_time != 0 {
+            // ToDo: what is the default value of time.time???
+            let send_time = AbsSendTimeExtension::new(
+                self.time_gen.map_or_else(|| Duration::default(), |v| v()),
+            );
 
-            packets[l - 1]
-                .header
-                .set_extension(self.abs_send_time, &raw)?;
+            // apply http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time
+            let b = match send_time.marshal() {
+                Ok(e) => e,
+                Err(e) => return Err(e),
+            };
+
+            let len = packets.len() - 1;
+
+            match packets[len].header.set_extension(self.abs_send_time, &b) {
+                Err(e) => return Err(e),
+
+                _ => {}
+            }
         }
 
-        Ok(packets)
+        return Ok(packets);
     }
 }
