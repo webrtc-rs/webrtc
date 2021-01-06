587353e68810583d7af3c0b2e72cccf3e81d0b77
Michael Uti <utimichael9@gmail.com>
2021-01-06 01:12:53 +0100

    errors: add defined errors

---
+++ crates/rtp/src/errors.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+pub enum RTPError {
+    HeaderSizeInsufficient(String),
+    HeaderSizeInsufficientForExtension(String),
+    RFC8285OneByteHeaderIDRange(String),
+    RFC8285OneByteHeaderSize(String),
+    RFC8285TwoByteHeaderIDRange(String),
+    RFC8285TwoByteHeaderSize(String),
+    RFC3550HeaderIDRange(String),
+    ShortBuffer,
+    HeaderExtensionNotEnabled,
+    HeaderExtensionNotFound,
+}
+
+impl std::fmt::Display for RTPError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "()")
+    }
+}
+
+impl std::error::Error for RTPError {}
