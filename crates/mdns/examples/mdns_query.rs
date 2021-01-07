b56cfae434ccbb9c19c323ef65f0931185cd90b9
Rain Liu <yuliu@outlook.com>
2021-01-07 00:54:19 -0800

    finish examples

--- crates/mdns/examples/mdns_query.rs
+++ crates/mdns/examples/mdns_query.rs
@@ -1,17 +1,27 @@
-use tokio::net::UdpSocket;
-use tokio::time;
+use webrtc_rs_mdns as mdns;
 
-use tokio::time::Duration;
+use mdns::{config::*, conn::*};
+
+use tokio::net::UdpSocket;
+
 use util::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    let buf = [1u8; 15000];
-    let count = 1473;
-    socket.send_to(&buf[0..count], "224.0.0.251:8888").await?;
-    println!("sending...");
-    time::sleep(Duration::from_secs(1)).await;
-    println!("done");
+    env_logger::Builder::new().init();
+
+    let socket = UdpSocket::bind("0.0.0.0:9999").await?;
+
+    let server = DNSConn::server(
+        socket,
+        Config {
+            dst_port: Some(8888),
+            ..Default::default()
+        },
+    )?;
+
+    let (answer, src) = server.query("webrtc-rs-mdns-1.local").await?;
+    println!("answer = {}, src = {}", answer, src);
+
     Ok(())
 }
--- crates/mdns/examples/mdns_server.rs
+++ crates/mdns/examples/mdns_server.rs
@@ -1,19 +1,29 @@
-use std::net::Ipv4Addr;
+use webrtc_rs_mdns as mdns;
+
+use mdns::{config::*, conn::*};
+
 use tokio::net::UdpSocket;
 
 use util::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let socket = UdpSocket::bind("0.0.0.0:8888").await?;
-    let mut buf = [0u8; 65535];
-    let multi_addr = Ipv4Addr::new(224, 0, 0, 251);
-    let inter = Ipv4Addr::new(0, 0, 0, 0);
-    socket.join_multicast_v4(multi_addr, inter)?;
-    println!("listening@{:?}...", socket.local_addr());
+    env_logger::Builder::new().init();
 
-    loop {
-        let (amt, src) = socket.recv_from(&mut buf).await?;
-        println!("received {} bytes from {:?}", amt, src);
-    }
+    let socket = UdpSocket::bind("0.0.0.0:8888").await?;
+    println!("socket.local_addr={:?}", socket.local_addr());
+
+    let _server = DNSConn::server(
+        socket,
+        Config {
+            dst_port: Some(9999),
+            local_names: vec![
+                "webrtc-rs-mdns-1.local".to_owned(),
+                "webrtc-rs-mdns-2.local".to_owned(),
+            ],
+            ..Default::default()
+        },
+    )?;
+
+    loop {}
 }
