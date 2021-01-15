58a78886d390486e4f2f980ec7c5e813387e795f
Ben Little <ben.little@benlittle.dev>
2021-01-15 17:17:34 +0000

    Refactor run_server()

--- tests/basic.rs
+++ tests/basic.rs
@@ -23,56 +23,6 @@ pub async fn random_port() -> u16 {
     local_addr.port()
 }
 
-#[test]
-pub fn e2e_basic() {
-    let cipher_suites: [u16; 2] = [
-        dtls::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-        dtls::TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA
-    ];
-    for cipher in cipher_suites.iter() {
-        let cert = Cert::new(CertConfig::new().self_signed());
-        let conf = Config::new()
-            .cipher_suite(*cipher)
-            .cert(cert)
-            .insecure_skip_verify();
-        check_comms(conf);
-    }
-}
-
-#[test]
-pub fn e2e_simple_psk() {
-    let cipher_suites: [CipherSuite; 3] = [
-        dtls::TLS_PSK_WITH_AES_128_CCM,
-        dtls::TLS_PSK_WITH_AES_128_CCM_8,
-        dtls::TLS_PSK_WITH_AES_128_GCM_SHA256,
-    ];
-    for cipher in cipher_suites.iter() {
-        let (psk, psk_id_hint) = create_psk();
-        let conf = Config::new()
-            .psk(psk, psk_id_hint)
-            .cipher_suite(*cipher);
-        check_comms(conf);
-    }      
-}
-
-#[test]
-pub fn e2e_mtu() {
-    let mtus: &'static [MTU; 3] = &[
-        10_000,
-        1000,
-        100
-    ];
-    for mtu in mtus.iter() {
-        let cert = Cert::new(CertConfig::new().self_signed().host("localhost"));
-        let conf: Config = Config::new()
-            .cert(cert)
-            .cipher_suite(dtls::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256)
-            .mtu(*mtu)
-            .insecure_skip_verify();
-        check_comms(conf);
-    }
-}
-
 pub async fn run_client(
     client_config: Config,
     server_port: u16,
@@ -99,57 +49,78 @@ pub async fn run_server(
     server_ready: tokio::sync::oneshot::Sender<()>,
     err_chan: tokio::sync::mpsc::Sender<std::io::Error>,
 ) {
-    match dtls::listen("udp", "127.0.0.1", server_port, server_config).await {
-        Ok(listener) => {
-            server_ready.send(());
-            match listener.accept().await {
-                Ok((stream, addr)) => {
-                    loop {
-                        match stream.readable().await {
-                            Ok(_) => {}
-                            Err(e) => { err_chan.send(e); return; }
-                        }
-                        let mut buf = [0; 8192];
-                        let n = match stream.try_read(&mut buf) {
-                            Ok(n) => n,
-                            Err(e) => { err_chan.send(e); break; }
-                        };
-                        match std::str::from_utf8(&buf[0..n]) {
-                            Ok(v) => {
-                                out_chan.send(v);
-                                loop {
-                                    match stream.writable().await {
-                                        Ok(_) => {}
-                                        Err(e) => { err_chan.send(e); return; }
-                                    }
-                                    match stream.try_write(TEST_MESSAGE.as_bytes()) {
-                                        Ok(n) => {
-                                            break;
-                                        }
-                                        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                                            continue;
-                                        }
-                                        Err(e) => { err_chan.send(e); return; }
-                                    }
-                                }
-                                break;
-                            },
-                            Err(e) => {
-                                err_chan.send(
-                                    std::io::Error::new(
-                                        std::io::ErrorKind::Other,
-                                        "failed to parse utf8"
-                                    )
-                                );
-                                return;
-                            }
-                        }
-                    }
-                },
-                Err(e) => { err_chan.send(e); return; }
+    let listener = match dtls::listen("udp", "127.0.0.1", server_port, server_config).await {
+        Ok(listener) => listener,
+        Err(e) => {
+            err_chan.send(e);
+            return;
+        }
+    };
+    server_ready.send(());
+    let (stream, addr) = match listener.accept().await {
+        Ok((stream, addr)) => (stream, addr),
+        Err(e) => {
+            err_chan.send(e);
+            return;
+        }
+    };
+    // TODO make sure addr is the expected client
+    let mut buf = vec![0_u8; 8192];
+    loop {
+        match stream.readable().await {
+            Ok(_) => {}
+            Err(e) => {
+                err_chan.send(e);
+                return;
             }
         }
-        Err(e) => { err_chan.send(e); return; }
+        match stream.try_read(&mut buf) {
+            Ok(n) => {
+                buf.truncate(n);
+                break;
+            },
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                err_chan.send(e);
+                return;
+            }
+        };
+    }
+    let s = match std::str::from_utf8(&buf) {
+        Ok(v) => v,
+        Err(e) => {
+            err_chan.send(
+                std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "failed to parse utf8"
+                )
+            );
+            return;
+        }
+    };
+    out_chan.send(s);
+    loop {
+        match stream.writable().await {
+            Ok(_) => { }
+            Err(e) => {
+                err_chan.send(e);
+                return;
+            }
+        }
+        match stream.try_write(TEST_MESSAGE.as_bytes()) {
+            Ok(n) => {
+                break;
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                err_chan.send(e);
+                break;
+            }
+        }
     }
 }
 
