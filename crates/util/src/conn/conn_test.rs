52fe7f3b333aff457984fb8228e1baa8079e29b2
Rain Liu <yuliu@outlook.com>
2021-01-21 23:20:46 -0800

    add lookup_host and upgrade to v0.1.4

---
+++ crates/util/src/conn/conn_test.rs
@@ -0,0 +1,22 @@
+use super::*;
+
+#[tokio::test]
+async fn test_conn_lookup_host() -> Result<()> {
+    let stun_serv_addr = "stun1.l.google.com:19302";
+
+    let ipv4_addr = lookup_host(true, stun_serv_addr).await?;
+    assert!(
+        ipv4_addr.is_ipv4(),
+        "expected ipv4 but got ipv6: {}",
+        ipv4_addr
+    );
+
+    let ipv6_addr = lookup_host(false, stun_serv_addr).await?;
+    assert!(
+        ipv6_addr.is_ipv6(),
+        "expected ipv6 but got ipv4: {}",
+        ipv6_addr
+    );
+
+    Ok(())
+}
--- crates/util/src/conn/mod.rs
+++ crates/util/src/conn/mod.rs
@@ -3,10 +3,13 @@ pub mod conn_udp;
 
 #[cfg(test)]
 mod conn_pipe_test;
+#[cfg(test)]
+mod conn_test;
 
 use async_trait::async_trait;
 use std::io::Result;
 use std::net::SocketAddr;
+use tokio::net::ToSocketAddrs;
 
 #[async_trait]
 pub trait Conn {
@@ -16,4 +19,23 @@ pub trait Conn {
     async fn send(&self, buf: &[u8]) -> Result<usize>;
     async fn send_to(&self, buf: &[u8], target: SocketAddr) -> Result<usize>;
     fn local_addr(&self) -> Result<SocketAddr>;
+}
+
+pub async fn lookup_host<T>(use_ipv4: bool, host: T) -> Result<SocketAddr>
+where
+    T: ToSocketAddrs,
+{
+    for remote_addr in tokio::net::lookup_host(host).await? {
+        if (use_ipv4 && remote_addr.is_ipv4()) || (!use_ipv4 && remote_addr.is_ipv6()) {
+            return Ok(remote_addr);
+        }
+    }
+
+    Err(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        format!(
+            "No available {} IP address found!",
+            if use_ipv4 { "ipv4" } else { "ipv6" },
+        ),
+    ))
 }
