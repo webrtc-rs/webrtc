a0449ba6a6825efb460ceb2149d035e4a4dcde4c
Michael Uti <utimichael9@gmail.com>
2021-01-30 07:44:05 +0100

    Test
    Add audio level extension test

--- crates/rtp/src/extension/audio_level_extension/mod.rs
+++ crates/rtp/src/extension/audio_level_extension/mod.rs
@@ -28,7 +28,7 @@ const AUDIO_LEVEL_EXTENSION_SIZE: usize = 1;
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 // |      ID       |     len=1     |V|    level    |    0 (pad)    |
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Default)]
 pub struct AudioLevelExtension {
     pub level: u8,
     pub voice: bool,
