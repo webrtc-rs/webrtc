d096949e2ae88899ca1936231a8d77fcad99ee76
rainliu <yliu@webrtc.rs>
2021-04-11 09:21:31 -0700

    update Error with thiserror crate

--- crates/rtcp/src/error.rs
+++ crates/rtcp/src/error.rs
@@ -1,102 +1,89 @@
+use thiserror::Error;
+
 /// Possible RTCP error.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Error)]
 pub enum Error {
     /// Wrong marshal size.
+    #[error("Wrong marshal size")]
     WrongMarshalSize,
     /// Packet lost exceeds maximum amount of packets
     /// that can possibly be lost.
+    #[error("Invalid total lost count")]
     InvalidTotalLost,
     /// Packet contains an invalid header.
+    #[error( "Invalid header")]
     InvalidHeader,
     /// Packet contains empty compound.
+    #[error( "Empty compound packet")]
     EmptyCompound,
     /// Invalid first packet in compound packets. First packet
     /// should either be a SenderReport packet or ReceiverReport
+    #[error("First packet in compound must be SR or RR")]
     BadFirstPacket,
     /// CNAME was not defined.
+    #[error("Compound missing SourceDescription with CNAME")]
     MissingCNAME,
     /// Packet was defined before CNAME.
+    #[error("Feedback packet seen before CNAME")]
     PacketBeforeCNAME,
     /// Too many reports.
+    #[error("Too many reports")]
     TooManyReports,
     /// Too many chunks.
+    #[error("Too many chunks")]
     TooManyChunks,
     /// Too many sources.
+    #[error("too many sources")]
     TooManySources,
     /// Packet received is too short.
+    #[error("Packet status chunk must be 2 bytes")]
     PacketTooShort,
     /// Wrong packet type.
+    #[error("Wrong packet type")]
     WrongType,
     /// SDES received is too long.
+    #[error("SDES must be < 255 octets long")]
     SDESTextTooLong,
     /// SDES type is missing.
+    #[error("SDES item missing type")]
     SDESMissingType,
     /// Reason is too long.
+    #[error("Reason must be < 255 octets long")]
     ReasonTooLong,
     /// Invalid packet version.
+    #[error("Invalid packet version")]
     BadVersion,
     /// Invalid padding value.
+    #[error("Invalid padding value")]
     WrongPadding,
     /// Wrong feedback message type.
+    #[error("Wrong feedback message type")]
     WrongFeedbackType,
     /// Wrong payload type.
+    #[error("Wrong payload type")]
     WrongPayloadType,
     /// Header length is too small.
+    #[error("Header length is too small")]
     HeaderTooSmall,
     /// Media ssrc was defined as zero.
+    #[error("Media SSRC must be 0")]
     SSRCMustBeZero,
     /// Missing REMB identifier.
+    #[error("Missing REMB identifier")]
     MissingREMBIdentifier,
     /// SSRC number and length mismatches.
+    #[error("SSRC num and length do not match")]
     SSRCNumAndLengthMismatch,
     /// Invalid size or start index.
+    #[error("Invalid size or startIndex")]
     InvalidSizeOrStartIndex,
     /// Delta exceeds limit.
+    #[error("Delta exceed limit")]
     DeltaExceedLimit,
     /// Packet status chunk is not 2 bytes.
+    #[error("Packet status chunk must be 2 bytes")]
     PacketStatusChunkLength,
     /// Other undefined error.
+    #[error("Other errors {0}")]
     Other(String),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            Error::BadFirstPacket => write!(f, "First packet in compound must be SR or RR"),
-            Error::BadVersion => write!(f, "Invalid packet version"),
-            Error::DeltaExceedLimit => write!(f, "Delta exceed limit"),
-            Error::EmptyCompound => write!(f, "Empty compound packet"),
-            Error::HeaderTooSmall => write!(f, "Header length is too small"),
-            Error::InvalidHeader => write!(f, "Invalid header"),
-            Error::InvalidSizeOrStartIndex => {
-                write!(f, "Invalid size or startIndex")
-            }
-            Error::InvalidTotalLost => {
-                write!(f, "Invalid total lost count")
-            }
-            Error::MissingCNAME => write!(f, "Compound missing SourceDescription with CNAME"),
-            Error::MissingREMBIdentifier => write!(f, "Missing REMB identifier"),
-            Error::PacketBeforeCNAME => write!(f, "Feedback packet seen before CNAME"),
-            Error::PacketStatusChunkLength => {
-                write!(f, "Packet status chunk must be 2 bytes")
-            }
-            Error::PacketTooShort => write!(f, "Packet status chunk must be 2 bytes"),
-            Error::ReasonTooLong => write!(f, "Reason must be < 255 octets long"),
-            Error::SDESMissingType => write!(f, "SDES item missing type"),
-            Error::SDESTextTooLong => write!(f, "SDES must be < 255 octets long"),
-            Error::SSRCMustBeZero => write!(f, "Media SSRC must be 0"),
-            Error::SSRCNumAndLengthMismatch => {
-                write!(f, "SSRC num and length do not match")
-            }
-            Error::TooManyChunks => write!(f, "Too many chunks"),
-            Error::TooManyReports => write!(f, "Too many reports"),
-            Error::TooManySources => write!(f, "too many sources"),
-            Error::WrongFeedbackType => write!(f, "Wrong feedback message type"),
-            Error::WrongMarshalSize => write!(f, "Wrong marshal size"),
-            Error::WrongPadding => write!(f, "Invalid padding value"),
-            Error::WrongPayloadType => write!(f, "Wrong payload type"),
-            Error::WrongType => write!(f, "Wrong packet type"),
-            Error::Other(ref e) => write!(f, "{}", e),
-        }
-    }
 }
