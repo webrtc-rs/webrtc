2c298ee8653ec73f1ce3370d6d8e9ac965d952f7
rainliu <yliu@webrtc.rs>
2021-04-23 19:28:11 -0700

    refactoring sli

--- crates/rtcp/src/payload_feedbacks/slice_loss_indication/mod.rs
+++ crates/rtcp/src/payload_feedbacks/slice_loss_indication/mod.rs
@@ -1,15 +1,11 @@
-use std::fmt;
-
-use byteorder::{BigEndian, ByteOrder};
-use bytes::BytesMut;
-use header::Header;
-
-use super::{header, receiver_report};
-use crate::error::Error;
-use crate::packet::Packet;
-use crate::util::get_padding;
-
+#[cfg(test)]
 mod slice_loss_indication_test;
+
+use crate::{error::Error, header::*, packet::*, util::*};
+
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+use std::any::Any;
+use std::fmt;
 
 const SLI_LENGTH: usize = 2;
 const SLI_OFFSET: usize = 8;
@@ -48,36 +44,68 @@ impl fmt::Display for SliceLossIndication {
 }
 
 impl Packet for SliceLossIndication {
+    /// destination_ssrc returns an array of SSRC values that this packet refers to.
+    fn destination_ssrc(&self) -> Vec<u32> {
+        vec![self.media_ssrc]
+    }
+
+    fn size(&self) -> usize {
+        HEADER_LENGTH + SLI_OFFSET + self.sli_entries.len() * 4
+    }
+
+    /// Marshal encodes the SliceLossIndication in binary
+    fn marshal(&self) -> Result<Bytes, Error> {
+        if (self.sli_entries.len() + SLI_LENGTH) as u8 > std::u8::MAX {
+            return Err(Error::TooManyReports);
+        }
+
+        let mut writer = BytesMut::with_capacity(self.marshal_size());
+
+        let h = self.header();
+        let data = h.marshal()?;
+        writer.extend(data);
+
+        writer.put_u32(self.sender_ssrc);
+        writer.put_u32(self.media_ssrc);
+
+        for s in &self.sli_entries {
+            let sli = ((s.first as u32 & 0x1FFF) << 19)
+                | ((s.number as u32 & 0x1FFF) << 6)
+                | (s.picture as u32 & 0x3F);
+
+            writer.put_u32(sli);
+        }
+
+        put_padding(&mut writer);
+        Ok(writer.freeze())
+    }
+
     /// Unmarshal decodes the SliceLossIndication from binary
-    fn unmarshal(&mut self, raw_packet: &mut BytesMut) -> Result<(), Error> {
-        if raw_packet.len() < (header::HEADER_LENGTH + receiver_report::SSRC_LENGTH) {
+    fn unmarshal(raw_packet: &Bytes) -> Result<Self, Error> {
+        if raw_packet.len() < (HEADER_LENGTH + SSRC_LENGTH) {
             return Err(Error::PacketTooShort);
         }
 
-        let mut h = header::Header::default();
-        h.unmarshal(raw_packet)?;
+        let h = Header::unmarshal(raw_packet)?;
 
-        if raw_packet.len() < (header::HEADER_LENGTH + (4 * h.length as usize)) {
+        if raw_packet.len() < (HEADER_LENGTH + (4 * h.length as usize)) {
             return Err(Error::PacketTooShort);
         }
 
-        if h.packet_type != header::PacketType::TransportSpecificFeedback
-            || h.count != header::FORMAT_SLI
-        {
+        if h.packet_type != PacketType::TransportSpecificFeedback || h.count != FORMAT_SLI {
             return Err(Error::WrongType);
         }
 
-        self.sender_ssrc = BigEndian::read_u32(&raw_packet[header::HEADER_LENGTH..]);
-        self.media_ssrc = BigEndian::read_u32(
-            &raw_packet[header::HEADER_LENGTH + receiver_report::SSRC_LENGTH..],
-        );
+        let reader = &mut raw_packet.slice(HEADER_LENGTH..);
 
-        let mut i = header::HEADER_LENGTH + SLI_OFFSET;
+        let sender_ssrc = reader.get_u32();
+        let media_ssrc = reader.get_u32();
 
-        while i < header::HEADER_LENGTH + h.length as usize * 4 {
-            let sli = BigEndian::read_u32(&raw_packet[i..]);
-
-            self.sli_entries.push(SliEntry {
+        let mut i = HEADER_LENGTH + SLI_OFFSET;
+        let mut sli_entries = vec![];
+        while i < HEADER_LENGTH + h.length as usize * 4 {
+            let sli = reader.get_u32();
+            sli_entries.push(SliEntry {
                 first: ((sli >> 19) & 0x1FFF) as u16,
                 number: ((sli >> 6) & 0x1FFF) as u16,
                 picture: (sli & 0x3F) as u8,
@@ -86,46 +114,22 @@ impl Packet for SliceLossIndication {
             i += 4;
         }
 
-        Ok(())
+        Ok(SliceLossIndication {
+            sender_ssrc,
+            media_ssrc,
+            sli_entries,
+        })
     }
 
-    /// Marshal encodes the SliceLossIndication in binary
-    fn marshal(&self) -> Result<BytesMut, Error> {
-        if (self.sli_entries.len() + SLI_LENGTH) as u8 > std::u8::MAX {
-            return Err(Error::TooManyReports);
-        }
-
-        let mut raw_packet = BytesMut::new();
-        raw_packet.resize(SLI_OFFSET + (self.sli_entries.len() * 4), 0u8);
-
-        BigEndian::write_u32(&mut raw_packet, self.sender_ssrc);
-        BigEndian::write_u32(&mut raw_packet[4..], self.media_ssrc);
-
-        for (i, s) in self.sli_entries.iter().enumerate() {
-            let sli = ((s.first as u32 & 0x1FFF) << 19)
-                | ((s.number as u32 & 0x1FFF) << 6)
-                | (s.picture as u32 & 0x3F);
-
-            BigEndian::write_u32(&mut raw_packet[SLI_OFFSET + (4 * i)..], sli);
-        }
-
-        let mut header_data = self.header().marshal()?;
-
-        header_data.extend(&raw_packet);
-
-        Ok(header_data)
-    }
-
-    /// destination_ssrc returns an array of SSRC values that this packet refers to.
-    fn destination_ssrc(&self) -> Vec<u32> {
-        vec![self.media_ssrc]
-    }
-
-    fn trait_eq(&self, other: &dyn Packet) -> bool {
+    fn equal_to(&self, other: &dyn Packet) -> bool {
         other
             .as_any()
             .downcast_ref::<SliceLossIndication>()
             .map_or(false, |a| self == a)
+    }
+
+    fn clone_to(&self) -> Box<dyn Packet> {
+        Box::new(self.clone())
     }
 
     fn as_any(&self) -> &dyn Any {
@@ -134,19 +138,13 @@ impl Packet for SliceLossIndication {
 }
 
 impl SliceLossIndication {
-    fn len(&self) -> usize {
-        header::HEADER_LENGTH + SLI_OFFSET + self.sli_entries.len() * 4
-    }
-
     // Header returns the Header associated with this packet.
     pub fn header(&self) -> Header {
-        let l = self.len() + get_padding(self.len());
-
         Header {
-            padding: get_padding(self.len()) != 0,
-            count: header::FORMAT_SLI,
-            packet_type: header::PacketType::TransportSpecificFeedback,
-            length: ((l / 4) - 1) as u16,
+            padding: get_padding(self.size()) != 0,
+            count: FORMAT_SLI,
+            packet_type: PacketType::TransportSpecificFeedback,
+            length: ((self.marshal_size() / 4) - 1) as u16,
         }
     }
 }
