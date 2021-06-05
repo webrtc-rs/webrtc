ed68759a69fd40369ba9621576009bfff40550e1
rainliu <yliu@webrtc.rs>
2021-06-05 11:03:40 -0700

    add code structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+#![warn(rust_2018_idioms)]
+#![allow(dead_code)]
+
+//#[macro_use]
+//extern crate lazy_static;
+
+pub mod api;
+pub mod data;
+mod dtls;
+mod ice;
+pub mod media;
+pub mod peer;
+mod rtcp;
+mod rtp;
+mod sctp;
+mod sdp;
+mod stats;
+mod track;
+
 #[cfg(test)]
 mod tests {
     #[test]
