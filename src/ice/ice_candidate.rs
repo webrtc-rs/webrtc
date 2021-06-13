5457af1c3d285a5057ba17b93e2214fe37596099
rainliu <yliu@webrtc.rs>
2021-06-12 21:11:52 -0700

    add ice_candiate

--- src/ice/ice_candidate.rs
+++ src/ice/ice_candidate.rs
@@ -1,6 +1,17 @@
+use crate::error::Error;
 use crate::ice::ice_candidate_type::ICECandidateType;
 use crate::ice::ice_protocol::ICEProtocol;
+use ice::agent::agent_internal::AgentInternal;
+use ice::candidate::candidate_base::CandidateBaseConfig;
+use ice::candidate::candidate_host::CandidateHostConfig;
+use ice::candidate::candidate_peer_reflexive::CandidatePeerReflexiveConfig;
+use ice::candidate::candidate_relay::CandidateRelayConfig;
+use ice::candidate::candidate_server_reflexive::CandidateServerReflexiveConfig;
+use ice::candidate::Candidate;
 use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 /// ICECandidate represents a ice candidate
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -18,134 +29,135 @@ pub struct ICECandidate {
     pub tcp_type: String,
 }
 
-/*
-// Conversion for package ice
-func newICECandidatesFromICE(iceCandidates []ice.Candidate) ([]ICECandidate, error) {
-    candidates := []ICECandidate{}
-
-    for _, i := range iceCandidates {
-        c, err := newICECandidateFromICE(i)
-        if err != nil {
-            return nil, err
-        }
-        candidates = append(candidates, c)
-    }
-
-    return candidates, nil
+/// Conversion for package ice
+pub(crate) fn ice_candidates_from_ice(
+    ice_candidates: &[Arc<dyn Candidate + Send + Sync>],
+) -> Vec<ICECandidate> {
+    ice_candidates.iter().map(|c| c.into()).collect()
 }
 
-func newICECandidateFromICE(i ice.Candidate) (ICECandidate, error) {
-    typ, err := convertTypeFromICE(i.Type())
-    if err != nil {
-        return ICECandidate{}, err
-    }
-    protocol, err := NewICEProtocol(i.NetworkType().NetworkShort())
-    if err != nil {
-        return ICECandidate{}, err
-    }
+impl From<&Arc<dyn Candidate + Send + Sync>> for ICECandidate {
+    fn from(c: &Arc<dyn Candidate + Send + Sync>) -> Self {
+        let typ: ICECandidateType = c.candidate_type().into();
+        let protocol = ICEProtocol::from(c.network_type().network_short().as_str());
+        let (related_address, related_port) = if let Some(ra) = c.related_address() {
+            (ra.address, ra.port)
+        } else {
+            (String::new(), 0)
+        };
 
-    c := ICECandidate{
-        stats_id:    i.ID(),
-        foundation: i.foundation(),
-        priority:   i.priority(),
-        address:    i.address(),
-        Protocol:   protocol,
-        Port:       uint16(i.Port()),
-        Component:  i.Component(),
-        Typ:        typ,
-        TCPType:    i.TCPType().String(),
-    }
-
-    if i.RelatedAddress() != nil {
-        c.RelatedAddress = i.RelatedAddress().address
-        c.RelatedPort = uint16(i.RelatedAddress().Port)
-    }
-
-    return c, nil
-}
-
-func (c ICECandidate) toICE() (ice.Candidate, error) {
-    candidateID := c.stats_id
-    switch c.Typ {
-    case ICECandidateTypeHost:
-        config := ice.CandidateHostConfig{
-            CandidateID: candidateID,
-            Network:     c.Protocol.String(),
-            address:     c.address,
-            Port:        int(c.Port),
-            Component:   c.Component,
-            TCPType:     ice.NewTCPType(c.TCPType),
-            foundation:  c.foundation,
-            priority:    c.priority,
+        ICECandidate {
+            stats_id: c.id(),
+            foundation: c.foundation(),
+            priority: c.priority(),
+            address: c.address(),
+            protocol,
+            port: c.port(),
+            component: c.component(),
+            typ,
+            tcp_type: c.tcp_type().to_string(),
+            related_address,
+            related_port,
         }
-        return ice.NewCandidateHost(&config)
-    case ICECandidateTypeSrflx:
-        config := ice.CandidateServerReflexiveConfig{
-            CandidateID: candidateID,
-            Network:     c.Protocol.String(),
-            address:     c.address,
-            Port:        int(c.Port),
-            Component:   c.Component,
-            foundation:  c.foundation,
-            priority:    c.priority,
-            RelAddr:     c.RelatedAddress,
-            RelPort:     int(c.RelatedPort),
-        }
-        return ice.NewCandidateServerReflexive(&config)
-    case ICECandidateTypePrflx:
-        config := ice.CandidatePeerReflexiveConfig{
-            CandidateID: candidateID,
-            Network:     c.Protocol.String(),
-            address:     c.address,
-            Port:        int(c.Port),
-            Component:   c.Component,
-            foundation:  c.foundation,
-            priority:    c.priority,
-            RelAddr:     c.RelatedAddress,
-            RelPort:     int(c.RelatedPort),
-        }
-        return ice.NewCandidatePeerReflexive(&config)
-    case ICECandidateTypeRelay:
-        config := ice.CandidateRelayConfig{
-            CandidateID: candidateID,
-            Network:     c.Protocol.String(),
-            address:     c.address,
-            Port:        int(c.Port),
-            Component:   c.Component,
-            foundation:  c.foundation,
-            priority:    c.priority,
-            RelAddr:     c.RelatedAddress,
-            RelPort:     int(c.RelatedPort),
-        }
-        return ice.NewCandidateRelay(&config)
-    default:
-        return nil, fmt.Errorf("%w: %s", errICECandidateTypeUnknown, c.Typ)
     }
 }
 
-func convertTypeFromICE(t ice.CandidateType) (ICECandidateType, error) {
-    switch t {
-    case ice.CandidateTypeHost:
-        return ICECandidateTypeHost, nil
-    case ice.CandidateTypeServerReflexive:
-        return ICECandidateTypeSrflx, nil
-    case ice.CandidateTypePeerReflexive:
-        return ICECandidateTypePrflx, nil
-    case ice.CandidateTypeRelay:
-        return ICECandidateTypeRelay, nil
-    default:
-        return ICECandidateType(t), fmt.Errorf("%w: %s", errICECandidateTypeUnknown, t)
+impl ICECandidate {
+    pub(crate) async fn to_ice(
+        &self,
+        agent_internal: Option<Arc<Mutex<AgentInternal>>>,
+    ) -> Result<impl Candidate, Error> {
+        let candidate_id = self.stats_id.clone();
+        let c = match self.typ {
+            ICECandidateType::Host => {
+                let config = CandidateHostConfig {
+                    base_config: CandidateBaseConfig {
+                        candidate_id,
+                        network: self.protocol.to_string(),
+                        address: self.address.clone(),
+                        port: self.port,
+                        component: self.component,
+                        //tcp_type: ice.NewTCPType(c.TCPType),
+                        foundation: self.foundation.clone(),
+                        priority: self.priority,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                };
+                config.new_candidate_host(agent_internal).await?
+            }
+            ICECandidateType::Srflx => {
+                let config = CandidateServerReflexiveConfig {
+                    base_config: CandidateBaseConfig {
+                        candidate_id,
+                        network: self.protocol.to_string(),
+                        address: self.address.clone(),
+                        port: self.port,
+                        component: self.component,
+                        foundation: self.foundation.clone(),
+                        priority: self.priority,
+                        ..Default::default()
+                    },
+                    rel_addr: self.related_address.clone(),
+                    rel_port: self.related_port,
+                };
+                config
+                    .new_candidate_server_reflexive(agent_internal)
+                    .await?
+            }
+            ICECandidateType::Prflx => {
+                let config = CandidatePeerReflexiveConfig {
+                    base_config: CandidateBaseConfig {
+                        candidate_id,
+                        network: self.protocol.to_string(),
+                        address: self.address.clone(),
+                        port: self.port,
+                        component: self.component,
+                        foundation: self.foundation.clone(),
+                        priority: self.priority,
+                        ..Default::default()
+                    },
+                    rel_addr: self.related_address.clone(),
+                    rel_port: self.related_port,
+                };
+                config.new_candidate_peer_reflexive(agent_internal).await?
+            }
+            ICECandidateType::Relay => {
+                let config = CandidateRelayConfig {
+                    base_config: CandidateBaseConfig {
+                        candidate_id,
+                        network: self.protocol.to_string(),
+                        address: self.address.clone(),
+                        port: self.port,
+                        component: self.component,
+                        foundation: self.foundation.clone(),
+                        priority: self.priority,
+                        ..Default::default()
+                    },
+                    rel_addr: self.related_address.clone(),
+                    rel_port: self.related_port,
+                    relay_client: None, //TODO?
+                };
+                config.new_candidate_relay(agent_internal).await?
+            }
+            _ => return Err(Error::ErrICECandidateTypeUnknown),
+        };
+
+        Ok(c)
     }
 }
 
-func (c ICECandidate) String() string {
-    ic, err := c.toICE()
-    if err != nil {
-        return fmt.Sprintf("%#v failed to convert to ICE: %s", c, err)
+impl fmt::Display for ICECandidate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {} {}:{}{}",
+            self.protocol, self.typ, self.address, self.port, self.related_address,
+        )
     }
-    return ic.String()
 }
 
+/*TODO:
 // ToJSON returns an ICECandidateInit
 // as indicated by the spec https://w3c.github.io/webrtc-pc/#dom-rtcicecandidate-tojson
 func (c ICECandidate) ToJSON() ICECandidateInit {
