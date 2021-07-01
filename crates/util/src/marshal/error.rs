dd216014bcdbdf5b2ceb9d335b4179e097049aba
rainliu <yliu@webrtc.rs>
2021-07-01 14:45:08 -0700

    change Marshal trait's marshal_to as &mut [u8]

---
+++ crates/util/src/marshal/error.rs
@@ -0,0 +1,14 @@
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum Error {
+    #[allow(non_camel_case_types)]
+    #[error("{0}")]
+    new(String),
+}
+
+impl Error {
+    pub fn equal(&self, err: &anyhow::Error) -> bool {
+        err.downcast_ref::<Self>().map_or(false, |e| e == self)
+    }
+}
--- crates/util/src/marshal/mod.rs
+++ crates/util/src/marshal/mod.rs
@@ -1,21 +1,27 @@
+pub mod error;
 pub mod exact_size_buf;
 
 use anyhow::Result;
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use bytes::{Buf, Bytes, BytesMut};
+use error::Error;
 
 pub trait MarshalSize {
     fn marshal_size(&self) -> usize;
 }
 
 pub trait Marshal: MarshalSize {
-    fn marshal_to<B>(&self, buf: &mut B) -> Result<usize>
-    where
-        B: BufMut;
+    fn marshal_to(&self, buf: &mut [u8]) -> Result<usize>;
 
     fn marshal(&self) -> Result<Bytes> {
-        let mut buf = BytesMut::with_capacity(self.marshal_size());
-        let _ = self.marshal_to(&mut buf)?;
-        Ok(buf.freeze())
+        let l = self.marshal_size();
+        let mut buf = BytesMut::with_capacity(l);
+        buf.resize(l, 0);
+        let n = self.marshal_to(&mut buf)?;
+        if n != l {
+            Err(Error::new(format!("marshal_to output size {}, but expect {}", n, l)).into())
+        } else {
+            Ok(buf.freeze())
+        }
     }
 }
 
