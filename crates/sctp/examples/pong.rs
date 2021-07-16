49c8986806ef5e10c1c379932f9021ebeffdc86c
rainliu <yliu@webrtc.rs>
2021-07-15 20:38:54 -0700

    update dep and upgrade self to v0.2.3

--- crates/sctp/examples/pong.rs
+++ crates/sctp/examples/pong.rs
@@ -2,17 +2,14 @@ use webrtc_sctp::association::*;
 use webrtc_sctp::stream::*;
 
 use anyhow::Result;
-use async_trait::async_trait;
 use bytes::Bytes;
 use clap::{App, AppSettings, Arg};
-use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::UdpSocket;
 use tokio::signal;
 use tokio::sync::mpsc;
-use tokio::sync::Mutex;
-use util::Conn;
+use util::{conn::conn_disconnected_packet::DisconnectedPacketConn, Conn};
 
 // RUST_LOG=trace cargo run --color=always --package webrtc-sctp --example pong -- --host 0.0.0.0:5678
 
@@ -108,58 +105,4 @@ async fn main() -> Result<()> {
     let _ = done_rx.recv().await;
 
     Ok(())
-}
-
-/// Reference: https://github.com/pion/sctp/blob/master/association_test.go
-/// Since UDP is connectionless, as a server, it doesn't know how to reply
-/// simply using the `Write` method. So, to make it work, `disconnectedPacketConn`
-/// will infer the last packet that it reads as the reply address for `Write`
-struct DisconnectedPacketConn {
-    raddr: Mutex<SocketAddr>,
-    pconn: Arc<dyn Conn + Send + Sync>,
-}
-
-impl DisconnectedPacketConn {
-    fn new(conn: Arc<dyn Conn + Send + Sync>) -> impl Conn {
-        DisconnectedPacketConn {
-            raddr: Mutex::new(SocketAddr::new(Ipv4Addr::new(0, 0, 0, 0).into(), 0)),
-            pconn: conn,
-        }
-    }
-}
-
-#[async_trait]
-impl Conn for DisconnectedPacketConn {
-    async fn connect(&self, addr: SocketAddr) -> Result<()> {
-        self.pconn.connect(addr).await
-    }
-
-    async fn recv(&self, buf: &mut [u8]) -> Result<usize> {
-        let (n, addr) = self.pconn.recv_from(buf).await?;
-        {
-            let mut raddr = self.raddr.lock().await;
-            *raddr = addr;
-        }
-        Ok(n)
-    }
-
-    async fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-        self.pconn.recv_from(buf).await
-    }
-
-    async fn send(&self, buf: &[u8]) -> Result<usize> {
-        let addr = {
-            let raddr = self.raddr.lock().await;
-            *raddr
-        };
-        self.pconn.send_to(buf, addr).await
-    }
-
-    async fn send_to(&self, buf: &[u8], target: SocketAddr) -> Result<usize> {
-        self.pconn.send_to(buf, target).await
-    }
-
-    async fn local_addr(&self) -> Result<SocketAddr> {
-        self.pconn.local_addr().await
-    }
 }
