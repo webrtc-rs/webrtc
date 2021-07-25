bfa3465eba5021d43ef366c325552c75bf003c3f
Vincent Esche <regexident@gmail.com>
2021-07-26 00:29:24 +0200

    Add basic implementation of audio buffer and sample

---
+++ crates/media/benches/audio_buffer.rs
@@ -0,0 +1,39 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use webrtc_media::audio::buffer::{
+    layout::{Deinterleaved, Interleaved},
+    Buffer,
+};
+
+fn benchmark_from(c: &mut Criterion) {
+    type Sample = i32;
+    let channels = 4;
+    let frames = 100_000;
+    let deinterleaved_buffer: Buffer<Sample, Deinterleaved> = {
+        let samples = (0..(channels * frames)).map(|i| i as i32).collect();
+        Buffer::new(samples, channels)
+    };
+    let interleaved_buffer: Buffer<Sample, Interleaved> = {
+        let samples = (0..(channels * frames)).map(|i| i as i32).collect();
+        Buffer::new(samples, channels)
+    };
+
+    c.bench_function("Buffer<T, Interleaved> => Buffer<T, Deinterleaved>", |b| {
+        b.iter(|| {
+            black_box(Buffer::<Sample, Interleaved>::from(
+                deinterleaved_buffer.as_ref(),
+            ));
+        })
+    });
+
+    c.bench_function("Buffer<T, Deinterleaved> => Buffer<T, Interleaved>", |b| {
+        b.iter(|| {
+            black_box(Buffer::<Sample, Deinterleaved>::from(
+                interleaved_buffer.as_ref(),
+            ));
+        })
+    });
+}
+
+criterion_group!(benches, benchmark_from);
+criterion_main!(benches);
