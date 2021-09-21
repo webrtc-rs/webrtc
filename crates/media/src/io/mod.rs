39af71dbb6f70969bbe7a47a607dac01d4046d37
Rain Liu <yliu@webrtc.rs>
2021-09-21 15:08:06 -0700

    add Sample

--- crates/media/src/io/mod.rs
+++ crates/media/src/io/mod.rs
@@ -1,19 +1,9 @@
-use anyhow::Result;
-use std::time::{Duration, SystemTime};
-
 pub mod ivf_reader;
 pub mod ivf_writer;
 pub mod ogg_reader;
 pub mod ogg_writer;
 
-/// A Sample contains encoded media and timing information
-pub struct Sample {
-    pub data: Vec<u8>,
-    pub timestamp: SystemTime,
-    pub duration: Duration,
-    pub packet_timestamp: u32,
-    pub prev_dropped_packets: u16,
-}
+use anyhow::Result;
 
 // RTPWriter defines an interface to handle
 // the creation of media files
--- crates/media/src/lib.rs
+++ crates/media/src/lib.rs
@@ -1,4 +1,31 @@
+#![warn(rust_2018_idioms)]
+#![allow(dead_code)]
+
 pub mod audio;
 pub mod error;
 pub mod io;
 pub mod video;
+
+use bytes::Bytes;
+use std::time::{Duration, SystemTime};
+
+/// A Sample contains encoded media and timing information
+pub struct Sample {
+    pub data: Bytes,
+    pub timestamp: SystemTime,
+    pub duration: Duration,
+    pub packet_timestamp: u32,
+    pub prev_dropped_packets: u16,
+}
+
+impl Default for Sample {
+    fn default() -> Self {
+        Sample {
+            data: Bytes::new(),
+            timestamp: SystemTime::now(),
+            duration: Duration::from_secs(0),
+            packet_timestamp: 0,
+            prev_dropped_packets: 0,
+        }
+    }
+}
