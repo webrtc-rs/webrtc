2e336bd772ee8477874cec6acb31b5b525bc4d0d
Rain Liu <yliu@webrtc.rs>
2021-10-09 20:32:05 -0700

    fix fuzz

--- crates/sdp/fuzz/fuzz_targets/parse_session.rs
+++ crates/sdp/fuzz/fuzz_targets/parse_session.rs
@@ -3,5 +3,5 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut cursor = std::io::Cursor::new(data);
-    let _session = sdp::session_description::SessionDescription::unmarshal(&mut cursor);
+    let _session = sdp::SessionDescription::unmarshal(&mut cursor);
 });
