0a3a15fcbb9f5e1b6284de7edfd488f78e46a92e
Eric Saxby <sax@livinginthepast.org>
2022-03-31 14:23:15 -0700

    Begin structuring code for collecting stats

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -1,1 +1,35 @@
+use stats_collector::StatsCollector;
+
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+pub mod stats_collector;
+
+pub enum StatsType {
+    CSRC,
+    CandidatePair,
+    Certificate,
+    Codec,
+    DataChannel,
+    InboundRTP,
+    LocalCandidate,
+    OutboundRTP,
+    PeerConnection,
+    Receiver,
+    RemoteCandidate,
+    RemoteInboundRTP,
+    RemoteOutboundRTP,
+    Sender,
+    Stream,
+    Track,
+    Transport,
+}
+
+pub struct StatsReport {}
+
+impl From<Arc<Mutex<StatsCollector>>> for StatsReport {
+    fn from(_collector: Arc<Mutex<StatsCollector>>) -> Self {
+        StatsReport {}
+    }
+}
 
---
+++ src/stats/stats_collector.rs
@@ -0,0 +1,7 @@
+pub struct StatsCollector {}
+
+impl StatsCollector {
+    pub(crate) fn new() -> Self {
+        StatsCollector {}
+    }
+}
