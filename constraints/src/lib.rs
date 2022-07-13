ad8ef3caba5ab7d700d6948b0489b296afe657ac
Vincent Esche <regexident@gmail.com>
2022-07-13 20:42:19 +0200

    Add module documentation header comment

--- constraints/src/lib.rs
+++ constraints/src/lib.rs
@@ -1,3 +1,7 @@
+//! Pure Rust implementation of the constraint logic defined in the ["Media Capture and Streams"][mediacapture_streams] spec.
+//!
+//! [mediacapture_streams]: https://www.w3.org/TR/mediacapture-streams/
+
 #[cfg(test)]
 mod tests {
     #[test]
