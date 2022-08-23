530b4b60427d99c151ab36a934c4d565843983c2
neonphog <neonphog@gmail.com>
2022-08-23 09:57:29 -0600

    [DTLS] Add NamedCurve::P384

--- dtls/src/curve/named_curve.rs
+++ dtls/src/curve/named_curve.rs
@@ -3,12 +3,13 @@ use rand_core::OsRng; // requires 'getrandom' feature
 use crate::error::*;
 
 // https://www.iana.org/assignments/tls-parameters/tls-parameters.xml#tls-parameters-8
+#[repr(u16)]
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum NamedCurve {
+    Unsupported = 0x0000,
     P256 = 0x0017,
     P384 = 0x0018,
     X25519 = 0x001d,
-    Unsupported,
 }
 
 impl From<u16> for NamedCurve {
@@ -24,6 +25,7 @@ impl From<u16> for NamedCurve {
 
 pub(crate) enum NamedCurvePrivateKey {
     EphemeralSecretP256(p256::ecdh::EphemeralSecret),
+    EphemeralSecretP384(p384::ecdh::EphemeralSecret),
     StaticSecretX25519(x25519_dalek::StaticSecret),
 }
 
@@ -43,6 +45,14 @@ fn elliptic_curve_keypair(curve: NamedCurve) -> Result<NamedCurveKeypair> {
                 NamedCurvePrivateKey::EphemeralSecretP256(secret_key),
             )
         }
+        NamedCurve::P384 => {
+            let secret_key = p384::ecdh::EphemeralSecret::random(&mut OsRng);
+            let public_key = p384::EncodedPoint::from(secret_key.public_key());
+            (
+                public_key.as_bytes().to_vec(),
+                NamedCurvePrivateKey::EphemeralSecretP384(secret_key),
+            )
+        }
         NamedCurve::X25519 => {
             let secret_key = x25519_dalek::StaticSecret::new(OsRng);
             let public_key = x25519_dalek::PublicKey::from(&secret_key);
@@ -51,7 +61,6 @@ fn elliptic_curve_keypair(curve: NamedCurve) -> Result<NamedCurveKeypair> {
                 NamedCurvePrivateKey::StaticSecretX25519(secret_key),
             )
         }
-        //TODO: add NamedCurve::p384
         _ => return Err(Error::ErrInvalidNamedCurve),
     };
 
@@ -65,10 +74,9 @@ fn elliptic_curve_keypair(curve: NamedCurve) -> Result<NamedCurveKeypair> {
 impl NamedCurve {
     pub fn generate_keypair(&self) -> Result<NamedCurveKeypair> {
         match *self {
-            //TODO: add P384
             NamedCurve::X25519 => elliptic_curve_keypair(NamedCurve::X25519),
             NamedCurve::P256 => elliptic_curve_keypair(NamedCurve::P256),
-            //NamedCurve::P384 => elliptic_curve_keypair(NamedCurve::P384),
+            NamedCurve::P384 => elliptic_curve_keypair(NamedCurve::P384),
             _ => Err(Error::ErrInvalidNamedCurve),
         }
     }
