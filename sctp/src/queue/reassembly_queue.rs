95f8c26007a8f6c8354cb7b56385e5619a534ba7
Christopher Corley <cscorley@gmail.com>
2023-01-17 06:14:33 -0500

    sctp: fix 'attempt to add with overflow' panic in dev profile (#393)

--- sctp/src/queue/reassembly_queue.rs
+++ sctp/src/queue/reassembly_queue.rs
@@ -267,7 +267,8 @@ impl ReassemblyQueue {
                 return Err(Error::ErrTryAgain);
             }
             if cset.ssn == self.next_ssn {
-                self.next_ssn += 1;
+                // From RFC 4960 Sec 6.5:
+                self.next_ssn = self.next_ssn.wrapping_add(1);
             }
             self.ordered.remove(0)
         } else {
@@ -314,7 +315,7 @@ impl ReassemblyQueue {
 
         // Finally, forward next_ssn
         if sna16lte(self.next_ssn, last_ssn) {
-            self.next_ssn = last_ssn + 1;
+            self.next_ssn = last_ssn.wrapping_add(1);
         }
     }
 
