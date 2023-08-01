8a061f92ca048528cf7e5c3d4aa0a7553cbd2875
logist322 <hytornoy-99@mail.ru>
2023-08-01 14:44:12 +0200

    WIP

--- srtp/benches/srtp_bench.rs
+++ srtp/benches/srtp_bench.rs
@@ -5,29 +5,47 @@ use webrtc_srtp::{context::Context, protection_profile::ProtectionProfile};
 
 fn benchmark_buffer(c: &mut Criterion) {
     let mut ctx = Context::new(
-        &vec![0; 16],
-        &vec![0; 14],
+        &vec![
+            96, 180, 31, 4, 119, 137, 128, 252, 75, 194, 252, 44, 63, 56, 61, 55,
+        ],
+        &vec![247, 26, 49, 94, 99, 29, 79, 94, 5, 111, 252, 216, 62, 195],
         ProtectionProfile::Aes128CmHmacSha1_80,
         None,
         None,
-    ).unwrap();
+    )
+    .unwrap();
+
     let mut pld = BytesMut::new();
     for i in 0..1000 {
         pld.extend_from_slice(&[i as u8]);
     }
-    let pkt = rtp::packet::Packet {
-        header: rtp::header::Header {
-            sequence_number: 322,
-            ..Default::default()
-        },
-        payload: pld.into(),
-    };
-    let pkt_raw = pkt.marshal().unwrap();
+
+    let mut count = 1;
 
     c.bench_function("Benchmark context ", |b| {
-        b.iter(|| {
-            ctx.encrypt_rtp(&pkt_raw).unwrap();
-        });
+        b.iter_batched(
+            || {
+                let pkt = rtp::packet::Packet {
+                    header: rtp::header::Header {
+                        sequence_number: count,
+                        timestamp: count.into(),
+                        extension_profile: 48862,
+                        marker: true,
+                        padding: false,
+                        extension: true,
+                        payload_type: 96,
+                        ..Default::default()
+                    },
+                    payload: pld.clone().into(),
+                };
+                count += 1;
+                pkt.marshal().unwrap()
+            },
+            |pkt_raw| {
+                ctx.encrypt_rtp(&pkt_raw).unwrap();
+            },
+            criterion::BatchSize::LargeInput,
+        );
     });
 }
 
--- srtp/src/main.rs
+++ srtp/src/main.rs
@@ -4,26 +4,35 @@ use webrtc_srtp::{context::Context, protection_profile::ProtectionProfile};
 
 fn main() {
     let mut ctx = Context::new(
-        &vec![0; 16],
-        &vec![0; 14],
+        &vec![
+            96, 180, 31, 4, 119, 137, 128, 252, 75, 194, 252, 44, 63, 56, 61, 55,
+        ],
+        &vec![247, 26, 49, 94, 99, 29, 79, 94, 5, 111, 252, 216, 62, 195],
         ProtectionProfile::Aes128CmHmacSha1_80,
         None,
         None,
-    ).unwrap();
+    )
+    .unwrap();
     let mut pld = BytesMut::new();
     for i in 0..1000 {
         pld.extend_from_slice(&[i as u8]);
     }
-    let pkt = rtp::packet::Packet {
-        header: rtp::header::Header {
-            sequence_number: 322,
-            ..Default::default()
-        },
-        payload: pld.into(),
-    };
-    let pkt_raw = pkt.marshal().unwrap();
 
-    for _ in 0..10000 {
-        ctx.encrypt_rtp(&pkt_raw).unwrap();
+    for i in 1..=100000u32 {
+        let pkt = rtp::packet::Packet {
+            header: rtp::header::Header {
+                sequence_number: i as u16,
+                timestamp: i,
+                extension_profile: 48862,
+                marker: true,
+                padding: false,
+                extension: true,
+                payload_type: 96,
+                ..Default::default()
+            },
+            payload: pld.clone().into(),
+        };
+
+        ctx.encrypt_rtp(&pkt.marshal().unwrap()).unwrap();
     }
 }
