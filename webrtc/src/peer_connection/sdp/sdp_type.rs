f9518b684be52cac3f5dda36aebbbd83af36136f
Kenaniah Cerny <kenaniah@gmail.com>
2023-09-10 14:22:13 -0700

    docs

--- webrtc/src/peer_connection/sdp/sdp_type.rs
+++ webrtc/src/peer_connection/sdp/sdp_type.rs
@@ -2,36 +2,35 @@ use std::fmt;
 
 use serde::{Deserialize, Serialize};
 
-/// SDPType describes the type of an SessionDescription.
+/// Describes the type of an SDP message
 #[derive(Default, Debug, PartialEq, Eq, Copy, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum RTCSdpType {
     #[default]
+    #[serde(rename = "Unspecified")]
     Unspecified = 0,
 
-    /// indicates that a description MUST be treated as an SDP offer.
-    #[serde(rename = "offer")]
+    /// Indicates that a description MUST be treated as an SDP offer.
     Offer,
 
-    /// indicates that a description MUST be treated as an
+    /// Indicates that a description MUST be treated as an
     /// SDP answer, but not a final answer. A description used as an SDP
     /// pranswer may be applied as a response to an SDP offer, or an update to
     /// a previously sent SDP pranswer.
-    #[serde(rename = "pranswer")]
     Pranswer,
 
-    /// indicates that a description MUST be treated as an SDP
+    /// Indicates that a description MUST be treated as an SDP
     /// final answer, and the offer-answer exchange MUST be considered complete.
     /// A description used as an SDP answer may be applied as a response to an
-    /// SDP offer or as an update to a previously sent SDP pranswer.    
-    #[serde(rename = "answer")]
+    /// SDP offer or as an update to a previously sent SDP pranswer.
     Answer,
 
-    /// indicates that a description MUST be treated as
+    /// Indicates that a description MUST be treated as
     /// canceling the current SDP negotiation and moving the SDP offer and
     /// answer back to what it was in the previous stable state. Note the
     /// local or remote SDP descriptions in the previous stable state could be
-    /// null if there has not yet been a successful offer-answer negotiation.
-    #[serde(rename = "rollback")]
+    /// `None` if there has not yet been a successful offer-answer negotiation.
+    /// An answer or pranswer cannot be rolled back.
     Rollback,
 }
 
