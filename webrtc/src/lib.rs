0564ff33d88c667e8450c6c4a7487ea5be8440ef
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2024-01-14 21:07:21 +0100

    sdp: simulcast: Properly parse and set 'a=simulcast:' (#528)
    This improves parsing and generation of SDP for simulcast attribute
    according to RFC8853.
    Support for alternative formats are not implemented 100%.
    
    Comparing to Pion implementation, this commit at least covers more, hopefully
    most usecases for webrtc-rs users.

--- webrtc/src/lib.rs
+++ webrtc/src/lib.rs
@@ -27,6 +27,7 @@ pub(crate) const UNSPECIFIED_STR: &str = "Unspecified";
 pub(crate) const RECEIVE_MTU: usize = 1460;
 
 pub(crate) const SDP_ATTRIBUTE_RID: &str = "rid";
+pub(crate) const SDP_ATTRIBUTE_SIMULCAST: &str = "simulcast";
 pub(crate) const GENERATED_CERTIFICATE_ORIGIN: &str = "WebRTC";
 pub(crate) const SDES_REPAIR_RTP_STREAM_ID_URI: &str =
     "urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id";
