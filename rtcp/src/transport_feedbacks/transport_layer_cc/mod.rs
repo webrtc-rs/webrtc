c0e30bc1e7acbf222e8405d7feaf82ce59747fe5
yngrtc <y@ngr.tc>
2024-06-15 22:41:09 -0700

    fix RTCP RecvDelta i16 bug

--- rtcp/src/transport_feedbacks/transport_layer_cc/mod.rs
+++ rtcp/src/transport_feedbacks/transport_layer_cc/mod.rs
@@ -331,7 +331,7 @@ impl MarshalSize for RecvDelta {
         // big delta
         if self.type_tcc_packet == SymbolTypeTcc::PacketReceivedLargeDelta
             && delta >= i16::MIN as i64
-            && delta <= u16::MAX as i64
+            && delta <= i16::MAX as i64
         {
             return 2;
         }
@@ -358,10 +358,10 @@ impl Marshal for RecvDelta {
         // big delta
         if self.type_tcc_packet == SymbolTypeTcc::PacketReceivedLargeDelta
             && delta >= i16::MIN as i64
-            && delta <= u16::MAX as i64
+            && delta <= i16::MAX as i64
             && buf.remaining_mut() >= 2
         {
-            buf.put_u16(delta as u16);
+            buf.put_i16(delta as i16);
             return Ok(2);
         }
 
