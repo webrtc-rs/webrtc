ecdceafc8ed7f254b21123861504061bd08039c1
bbaldino <bbaldino@gmail.com>
2024-06-17 12:35:45 -0700

    make helper methods on ProtectionProfile pub

--- srtp/src/protection_profile.rs
+++ srtp/src/protection_profile.rs
@@ -8,27 +8,27 @@ pub enum ProtectionProfile {
 }
 
 impl ProtectionProfile {
-    pub(crate) fn key_len(&self) -> usize {
+    pub fn key_len(&self) -> usize {
         match *self {
             ProtectionProfile::Aes128CmHmacSha1_80 | ProtectionProfile::AeadAes128Gcm => 16,
         }
     }
 
-    pub(crate) fn salt_len(&self) -> usize {
+    pub fn salt_len(&self) -> usize {
         match *self {
             ProtectionProfile::Aes128CmHmacSha1_80 => 14,
             ProtectionProfile::AeadAes128Gcm => 12,
         }
     }
 
-    pub(crate) fn auth_tag_len(&self) -> usize {
+    pub fn auth_tag_len(&self) -> usize {
         match *self {
             ProtectionProfile::Aes128CmHmacSha1_80 => 10, //CIPHER_AES_CM_HMAC_SHA1AUTH_TAG_LEN,
             ProtectionProfile::AeadAes128Gcm => 16,       //CIPHER_AEAD_AES_GCM_AUTH_TAG_LEN,
         }
     }
 
-    pub(crate) fn auth_key_len(&self) -> usize {
+    pub fn auth_key_len(&self) -> usize {
         match *self {
             ProtectionProfile::Aes128CmHmacSha1_80 => 20,
             ProtectionProfile::AeadAes128Gcm => 0,
