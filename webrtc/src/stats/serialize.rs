1d8d6913eba7904379d04599fe06ae56519e4b72
Marcin Porębski <marcin.p.porebski@gmail.com>
2024-08-06 20:41:11 +0200

    stats: Fix `Instant` deserialization

--- webrtc/src/stats/serialize.rs
+++ webrtc/src/stats/serialize.rs
@@ -20,31 +20,26 @@ pub mod instant_to_epoch_seconds {
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards");
 
-        let epoch_ms = epoch.as_millis() as f64 / 1000.0;
+        let epoch_s = epoch.as_millis() as f64 / 1000.0;
 
-        epoch_ms.serialize(serializer)
+        epoch_s.serialize(serializer)
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Instant, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let epoch_seconds: f64 = Deserialize::deserialize(deserializer)?;
-
-        let since_epoch = Duration::from_secs_f64(epoch_seconds);
+        let epoch_s = f64::deserialize(deserializer)?;
+        let epoch_duration = Duration::from_secs_f64(epoch_s);
 
         let system_now = SystemTime::now();
         let instant_now = Instant::now();
 
-        let deserialized_system_time = UNIX_EPOCH + since_epoch;
+        let duration_since_approx = system_now
+            .duration_since(UNIX_EPOCH + epoch_duration)
+            .expect("Time went backwards");
+        let instant = instant_now - duration_since_approx;
 
-        let adjustment = match deserialized_system_time.duration_since(system_now) {
-            Ok(duration) => -duration.as_secs_f64(),
-            Err(e) => e.duration().as_secs_f64(),
-        };
-
-        let adjusted_instant = instant_now + Duration::from_secs_f64(adjustment);
-
-        Ok(adjusted_instant)
+        Ok(instant)
     }
 }
