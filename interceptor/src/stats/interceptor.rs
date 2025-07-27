fd9172ade7bd5143837b928d63be470aacc598db
Xiaobo Liu <cppcoffee@gmail.com>
2025-07-27 18:40:37 +0800

    interceptor: use futures::future::join_all in stats interceptor
    The previous implementation iterated through a list of futures and awaited them one by one. This is inefficient as it doesn't allow the futures to run concurrently.
    
    This commit refactors the code to use `futures::future::join_all` to await all futures concurrently, which is more efficient. This also involved adding the `futures` crate as a dependency to the `interceptor` crate.
    
    Signed-off-by: Xiaobo Liu <cppcoffee@gmail.com>

--- interceptor/src/stats/interceptor.rs
+++ interceptor/src/stats/interceptor.rs
@@ -570,10 +570,8 @@ where
                     },
                 })
             });
-            for fut in futures {
-                // TODO: Use futures::join_all
-                let _ = fut.await;
-            }
+
+            futures::future::join_all(futures).await;
 
             let futures = sender_reports.into_iter().map(|sr| {
                 let rtt_ms = match (sr.dlrr_last_rr, sr.dlrr_delay_rr, sr.sr_packets_sent) {
@@ -593,10 +591,8 @@ where
                     },
                 })
             });
-            for fut in futures {
-                // TODO: Use futures::join_all
-                let _ = fut.await;
-            }
+
+            futures::future::join_all(futures).await;
         }
 
         Ok((pkts, attributes))
