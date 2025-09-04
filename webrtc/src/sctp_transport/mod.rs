536ea96be523813813803dad2adc783a5b3e6d98
Luca Muscat <lucamuscat@gmail.com>
2025-09-04 15:40:30 +0200

    Make use of the `max-message-size` SDP attribute in datachannels (#722)
    * [PC] Extract datachannel retrieval logic from `have_data_channel` in order to be able to reuse the logic
    
    * [webrtc] Extact the max message size from the remote parsed SDP
    
    * [webrtc] Remove unused field `RTCSctpTransport`, as the max message size is fed to the underlying association during the call to `start_sctp`
    
    * [webrtc] Update `calc_message_size` to return `u32` instead of `usize`, as the only consumer of this method expects `u32`
    
    * [webrtc] Allow users to specify the max payload they can send over sctp
    
    * [datachannel] Add the `max_message_size` config field into the datachannel's config
    
    Given that we are no longer fixed to the 64KB max size for message
    sizes, the user needs a way to know the actual max message size a
    datachannel may send before returning an error. This information is
    particularly useful for applications where large messages are chunked
    and framed in the application itself.
    
    * Basic test suite for `max-message-size` handling
    
    * [webrtc] Introduce `get_application_section_max_message_size`
    
    * Remove if disagreed upon

--- webrtc/src/sctp_transport/mod.rs
+++ webrtc/src/sctp_transport/mod.rs
@@ -69,10 +69,6 @@ pub struct RTCSctpTransport {
     // so we need a dedicated field
     is_started: AtomicBool,
 
-    // max_message_size represents the maximum size of data that can be passed to
-    // DataChannel's send() method.
-    max_message_size: usize,
-
     // max_channels represents the maximum amount of DataChannel's that can
     // be used simultaneously.
     max_channels: u16,
@@ -103,7 +99,6 @@ impl RTCSctpTransport {
             dtls_transport,
             state: AtomicU8::new(RTCSctpTransportState::Connecting as u8),
             is_started: AtomicBool::new(false),
-            max_message_size: RTCSctpTransport::calc_message_size(65536, 65536),
             max_channels: SCTP_MAX_CHANNELS,
             sctp_association: Mutex::new(None),
             on_error_handler: Arc::new(ArcSwapOption::empty()),
@@ -138,7 +133,7 @@ impl RTCSctpTransport {
     /// a connection over SCTP.
     pub async fn start(
         &self,
-        _remote_caps: SCTPTransportCapabilities,
+        remote_caps: SCTPTransportCapabilities,
         local_port: u16,
         remote_port: u16,
     ) -> Result<()> {
@@ -148,6 +143,12 @@ impl RTCSctpTransport {
         self.is_started.store(true, Ordering::SeqCst);
 
         let dtls_transport = self.transport();
+
+        let max_message_size = Self::calc_message_size(
+            remote_caps.max_message_size,
+            self.setting_engine.sctp_max_message_size_can_send.as_u32(),
+        );
+
         if let Some(net_conn) = &dtls_transport.conn().await {
             let sctp_association = loop {
                 tokio::select! {
@@ -162,7 +163,7 @@ impl RTCSctpTransport {
                     association = sctp::association::Association::client(sctp::association::Config {
                         net_conn: Arc::clone(net_conn) as Arc<dyn Conn + Send + Sync>,
                         max_receive_buffer_size: 0,
-                        max_message_size: 0,
+                        max_message_size,
                         name: String::new(),
                         local_port,
                         remote_port,
@@ -232,7 +233,10 @@ impl RTCSctpTransport {
                 _ = param.notify_rx.notified() => break,
                 result = DataChannel::accept(
                     &param.sctp_association,
-                    data::data_channel::Config::default(),
+                    data::data_channel::Config {
+                        max_message_size: param.sctp_association.max_message_size(),
+                        ..data::data_channel::Config::default()
+                    },
                     &existing_data_channels,
                 ) => {
                     match result {
@@ -338,9 +342,9 @@ impl RTCSctpTransport {
             .store(Some(Arc::new(Mutex::new(f))));
     }
 
-    fn calc_message_size(remote_max_message_size: usize, can_send_size: usize) -> usize {
+    fn calc_message_size(remote_max_message_size: u32, can_send_size: u32) -> u32 {
         if remote_max_message_size == 0 && can_send_size == 0 {
-            usize::MAX
+            u32::MAX
         } else if remote_max_message_size == 0 {
             can_send_size
         } else if can_send_size == 0 || can_send_size > remote_max_message_size {
