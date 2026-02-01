065151d42f70c2cb80c8fe192d29683acad8fa75
yngrtc <y@ngr.tc>
2026-02-01 08:49:20 -0800

    remove old webrtc code

--- src/peer_connection/policy/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod bundle_policy;
-pub mod ice_transport_policy;
-pub mod rtcp_mux_policy;
-pub mod sdp_semantics;
--- src/sctp_transport/sctp_transport_capabilities.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-/// SCTPTransportCapabilities indicates the capabilities of the SCTPTransport.
-#[derive(Debug, PartialEq, Eq, Copy, Clone, Serialize, Deserialize)]
-pub struct SCTPTransportCapabilities {
-    pub max_message_size: u32,
-}
